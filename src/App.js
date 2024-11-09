10aecc4e4755c0e3b41d5d55dd6059c4434ed026
Dew0277 <jaloliddina98@gmail.com>
2024-11-09 17:34:37 +0500

    create new Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./pages/home";
 
 function App() {
   return (
     <>
       <Header />
       <h1>App Pages</h1>
+      <Home />
     </>
   );
 }
